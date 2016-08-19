9f9e492267ea20f6a0fa27bd9e7fe8f7bfdb16d8
allen shiu <allen.shiu@ufispace.com>
2016-08-19 10:07:21 +0800

    Initial Commit

---
+++ CustomPresentAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomPresentAnimation
+//
+//  Created by AllenShiu on 2016/8/19.
+//  Copyright © 2016年 AllenShiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomPresentAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomPresentAnimation
+//
+//  Created by AllenShiu on 2016/8/19.
+//  Copyright © 2016年 AllenShiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
